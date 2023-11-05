cd7aeaa0a7b3ffb8231480444dcb75ee4427c228
choi seoin <inseo@uupfi.com>
2023-11-05 12:35:16 +0900

    initial commit

---
+++ src/test/kotlin/com/seoin/example/webclient/WebClientApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.seoin.example.webclient
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebClientApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
